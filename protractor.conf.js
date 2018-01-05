5b6cdddd86c8b39ab71511ee96e2a6b0afe0db2b
Ana Barroso <ana.barroso@keyidentity.com>
2018-01-05 14:52:56 +0100

    chore: set path to chromium binary
    refers to LINOTP-677
    
    Change-Id: I013b8ffa83882ea6cfb33e259ca8afa232e8ef78

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,7 +1,9 @@
 // Protractor configuration file, see link for more information
 // https://github.com/angular/protractor/blob/master/lib/config.ts
 
-const { SpecReporter } = require('jasmine-spec-reporter');
+const {
+  SpecReporter
+} = require('jasmine-spec-reporter');
 
 exports.config = {
   allScriptsTimeout: 11000,
@@ -9,7 +11,10 @@ exports.config = {
     './e2e/**/*.e2e-spec.ts'
   ],
   capabilities: {
-    'browserName': 'chromium'
+    'browserName': 'chrome',
+    'chromeOptions': {
+      binary: '/usr/bin/chromium'
+    }
   },
   directConnect: true,
   baseUrl: 'http://localhost:4200/',
@@ -17,12 +22,16 @@ exports.config = {
   jasmineNodeOpts: {
     showColors: true,
     defaultTimeoutInterval: 30000,
-    print: function() {}
+    print: function () {}
   },
   onPrepare() {
     require('ts-node').register({
       project: 'e2e/tsconfig.e2e.json'
     });
-    jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: true } }));
+    jasmine.getEnv().addReporter(new SpecReporter({
+      spec: {
+        displayStacktrace: true
+      }
+    }));
   }
 };
