e36b73ec194f1a8b661b82c8f7c54705ecb8b682
Lukas Engelter <lukas.engelter@keyidentity.com>
2018-01-19 14:19:54 +0100

    feat: add token description
    Change-Id: Ibe81a88b045dd425a1a3860c22355002f6584800

--- src/app/token.ts
+++ src/app/token.ts
@@ -1,9 +1,11 @@
 export class Token {
   id: string;
   type: string;
+  description: string;
 
-  constructor(id: string, type: string) {
+  constructor(id: string, type: string, description: string) {
     this.id = id;
     this.type = type;
+    this.description = description;
   }
 }
