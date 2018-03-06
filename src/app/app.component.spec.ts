ccf0587bbdcd7b5c6305b2fdd1a31ddd22d5d3e4
Ana Barroso <ana.barroso@keyidentity.com>
2018-03-06 09:26:13 +0100

    feat: apply material design to app title
    Change-Id: I9b18284226d1b971dfd48ffb40920d471f9f7f67

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,14 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { MaterialModule } from './material.module';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule
+        RouterTestingModule,
+        MaterialModule,
       ],
       declarations: [
         AppComponent
@@ -25,10 +27,10 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('KI Selfservice');
   }));
 
-  it('should render title in a h1 tag', async(() => {
+  it('should render title in a mat-toolbar tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('KI Selfservice');
+    expect(compiled.querySelector('mat-toolbar').textContent).toContain('KI Selfservice');
   }));
 });
