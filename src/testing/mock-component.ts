e3ba289343a556b90431810581752d9f1cd3646d
Lukas Engelter <lukas.engelter@keyidentity.com>
2018-03-14 14:24:17 +0100

    fix: add missing mock component to test
    Change-Id: I24a2c7cd45bee7dff2b9a66f653ee8d02e849a80

---
+++ src/testing/mock-component.ts
@@ -0,0 +1,22 @@
+import { Component, EventEmitter } from '@angular/core';
+
+export function MockComponent(options: Component & { attributes?: { [key: string]: any } }): Component {
+
+  const metadata: Component = {
+    selector: options.selector,
+    template: options.template || '',
+    inputs: options.inputs,
+    outputs: options.outputs || [],
+    exportAs: options.exportAs || ''
+  };
+
+  class Mock { }
+
+  Object.assign(Mock.prototype, options.attributes);
+
+  metadata.outputs.forEach(method => {
+    Mock.prototype[method] = new EventEmitter<any>();
+  });
+
+  return Component(metadata)(Mock as any);
+}
