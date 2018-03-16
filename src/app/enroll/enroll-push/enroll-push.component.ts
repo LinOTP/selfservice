2732e2dc1a50c9482de81df1a8e66d661811bc76
Ana Barroso <ana.barroso@keyidentity.com>
2018-03-16 09:09:26 +0100

    feat: start push activation functionality
    On initiating token activation, we display the QR code to the user.
    It contains the activation URL of the push token, provided by LinOTP
    after enrollment.
    
    Refers to LINOTP-669
    
    Change-Id: I478cd4b93fb344b5ef38c72fb0c054bca0e5f22b

--- src/app/enroll/enroll-push/enroll-push.component.ts
+++ src/app/enroll/enroll-push/enroll-push.component.ts
@@ -1,5 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 
+import { NgForm, FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MatStepper } from '@angular/material';
+import { MaterialModule } from '../../material.module';
+
+import { EnrollToken } from '../../token';
+import { TokenService } from '../../token.service';
+
+
+export interface EnrollPushToken extends EnrollToken {
+  type: 'push';
+  description: string;
+}
+
 @Component({
   selector: 'app-enroll-push',
   templateUrl: './enroll-push.component.html',
@@ -7,11 +20,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EnrollPushComponent implements OnInit {
 
-  description = '';
+  public enrollData: EnrollPushToken = {
+    type: 'push',
+    description: '',
+  };
 
-  constructor() { }
+  public enrolledToken: { serial: string, url: string };
+
+  constructor(
+    private tokenService: TokenService,
+  ) { }
 
   ngOnInit() {
   }
 
+  enroll(stepper: MatStepper) {
+    this.tokenService.enroll(this.enrollData).subscribe(response => {
+      if (response.result && response.result.value === true) {
+        this.enrolledToken = {
+          url: response.detail.lse_qr_url.value,
+          serial: response.detail.serial
+        };
+        stepper.next();
+      }
+    });
+  }
 }
