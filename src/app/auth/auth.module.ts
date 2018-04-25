14e8250642f3347b97c62009afecf3afec76c657
Lukas Engelter <lukas.engelter@keyidentity.com>
2018-04-25 12:17:10 +0200

    feat: auth interceptor added to catch unautherized requests
    On a 401 response we redirect the user to the login view.
    
    Change-Id: I805b0c9d4b8d3bab5ac5c5bf840d32d2241d668b

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthService } from './auth.service';
 import { AuthGuard } from './auth-guard.service';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { AuthInterceptor } from './auth-interceptor.service';
 
 @NgModule({
   imports: [
@@ -14,6 +15,11 @@ import { HttpClientModule } from '@angular/common/http';
   providers: [
     AuthService,
     AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true,
+    },
   ],
 })
 export class AuthModule { }
