3fa915c0ab351c4163b9e3bb6024efe7b2330f80
Andreas Rammhold <andreas.rammhold@keyidentity.com>
2019-04-29 11:38:26 +0200

    chore: use headless chrome for CI tests
    Replace PhantomJS, which is not supported anymore, with headless chrome.
    
    We must use ChromeHeadlessNoSandbox as we are running the tests as root
    in the container.

--- karma.conf.js
+++ karma.conf.js
@@ -9,7 +9,6 @@ module.exports = function (config) {
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
       require('karma-firefox-launcher'),
-      require('karma-phantomjs-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma')
@@ -21,13 +20,20 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, 'coverage'), reports: ['html', 'lcovonly', 'text', 'text-summary'],
       fixWebpackSourcePaths: true
     },
-    
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome', 'Chromium', 'Firefox', 'PhantomJS'],
+    browsers: ['Chrome', 'Chromium', 'ChromeHeadless', 'ChromeHeadlessNoSandbox', 'Firefox'],
+    customLaunchers: {
+      ChromeHeadlessNoSandbox: {
+        base: 'ChromeHeadless',
+        flags: [
+          '--no-sandbox'
+        ]
+      }
+    },
     singleRun: false
   });
 };
