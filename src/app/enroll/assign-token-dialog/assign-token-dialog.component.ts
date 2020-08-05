26ae4324962daedbc5fcd0725a42e2eae7346a58
Ana Barroso <ana.barroso@keyidentity.com>
2020-08-05 13:22:18 +0000

    feature: retrieve token serial by OTP in assignment dialog
    When assigning a token, the user now has the option to find the serial
    by entering an OTP.

--- src/app/enroll/assign-token-dialog/assign-token-dialog.component.ts
+++ src/app/enroll/assign-token-dialog/assign-token-dialog.component.ts
@@ -1,11 +1,13 @@
 import { Component, OnInit, ViewChild, Inject } from '@angular/core';
 import { FormGroup, FormBuilder, Validators } from '@angular/forms';
-import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { MatDialogRef, MAT_DIALOG_DATA, MatDialog } from '@angular/material/dialog';
 import { MatStepper } from '@angular/material/stepper';
 
 import { I18n } from '@ngx-translate/i18n-polyfill';
 
 import { EnrollmentService } from '../../api/enrollment.service';
+import { GetSerialDialogComponent } from '../../common/get-serial-dialog/get-serial-dialog.component';
+import { Permission } from '../../common/permissions';
 
 @Component({
   selector: 'app-assign-token-dialog',
@@ -17,6 +19,8 @@ export class AssignTokenDialogComponent implements OnInit {
   public assignmentForm: FormGroup;
   @ViewChild(MatStepper, { static: false }) public stepper: MatStepper;
 
+  public permissions = Permission;
+
   public success: boolean;
   public errorTypeMessage = '';
   public errorMessage = '';
@@ -26,6 +30,7 @@ export class AssignTokenDialogComponent implements OnInit {
   constructor(
     private dialogRef: MatDialogRef<AssignTokenDialogComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { closeLabel: string },
+    private dialog: MatDialog,
     private formBuilder: FormBuilder,
     private enrollmentService: EnrollmentService,
     private i18n: I18n,
@@ -80,4 +85,14 @@ export class AssignTokenDialogComponent implements OnInit {
       this.stepper.selectedIndex = 2;
     });
   }
+
+
+  public getSerial() {
+    this.dialog.open(GetSerialDialogComponent).afterClosed().subscribe(serial => {
+      if (serial) {
+        this.assignmentForm.controls.serial.setValue(serial);
+      }
+    });
+  }
+
 }
