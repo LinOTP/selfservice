ff8d92bc3257e75c63d22d05af4847e154b77c6f
Lukas Engelter <lukas.engelter@keyidentity.com>
2020-09-25 09:06:50 +0200

    refactor: token type details defintion out of the token service
    With angular v10 and the new i18n system it is now possible to have
    translations outside the injection system. This allows us to group all
    the constant definitions in the token.ts file

--- src/app/api/token.service.ts
+++ src/app/api/token.service.ts
@@ -5,8 +5,7 @@ import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 
 import { SessionService } from '../auth/session.service';
-import { Permission } from '../common/permissions';
-import { Token, TokenType, TokenTypeDetails } from './token';
+import { Token, TokenType, tokenTypeDetails, TokenTypeDetails, unknownTokenTypeDetail } from './token';
 import { LinOTPResponse } from './api';
 
 
@@ -52,118 +51,12 @@ export class TokenService {
     return t;
   }
 
-
-  get tokenTypeDetails(): TokenTypeDetails[] {
-    return [{
-      type: TokenType.PASSWORD,
-      name: $localize`password token`,
-      description: $localize`Personal text-based secret`,
-      icon: 'keyboard',
-      enrollmentPermission: Permission.ENROLLPASSWORD,
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Enter the token password`,
-    },
-    {
-      type: TokenType.HOTP,
-      name: $localize`soft token (event)`,
-      description: $localize`Event-based soft token (HOTP)`,
-      icon: 'cached',
-      enrollmentPermission: Permission.ENROLLHOTP,
-      enrollmentType: 'googleauthenticator',
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Enter OTP from event-based soft token`,
-    },
-    {
-      type: TokenType.TOTP,
-      name: $localize`soft token (time)`,
-      description: $localize`Time-based soft token (TOTP)`,
-      icon: 'timelapse',
-      enrollmentPermission: Permission.ENROLLTOTP,
-      enrollmentType: 'googleauthenticator_time',
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Enter OTP from time-based soft token`,
-    },
-    {
-      type: TokenType.PUSH,
-      name: $localize`Push-Token`,
-      description: $localize`Confirm authentication requests on your smartphone with the Authenticator app`,
-      icon: 'screen_lock_portrait',
-      enrollmentPermission: Permission.ENROLLPUSH,
-      activationPermission: Permission.ACTIVATEPUSH,
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Confirm the authentication using your smartphone`,
-    },
-    {
-      type: TokenType.QR,
-      name: $localize`QR-Token`,
-      description: $localize`Use the Authenticator app to scan QR code authentication requests`,
-      icon: 'qr_code',
-      enrollmentPermission: Permission.ENROLLQR,
-      activationPermission: Permission.ACTIVATEQR,
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Confirm the authentication by scanning a QR code`,
-    },
-    {
-      type: TokenType.MOTP,
-      name: $localize`mOTP token`,
-      description: $localize`Generate OTPs from your mobile device given a secret password and a custom pin`,
-      icon: 'stay_current_portrait',
-      enrollmentPermission: Permission.ENROLLMOTP,
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Enter OTP from mOTP token`,
-    },
-    {
-      type: TokenType.SMS,
-      name: $localize`SMS token`,
-      description: $localize`Receive an OTP via SMS`,
-      icon: 'textsms',
-      enrollmentPermission: Permission.ENROLLSMS,
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Enter OTP delivered via SMS`,
-    },
-    {
-      type: TokenType.EMAIL,
-      name: $localize`email token`,
-      description: $localize`Receive an OTP via email`,
-      icon: 'email',
-      enrollmentPermission: Permission.ENROLLEMAIL,
-      enrollmentActionLabel: $localize`Create`,
-      authenticationPrompt: $localize`Enter OTP delivered via email`,
-    },
-    {
-      type: TokenType.YUBICO,
-      name: $localize`YubiCloud token`,
-      description: $localize`Register your Yubikey to authenticate against the YubiCloud.`,
-      icon: 'vpn_key', // TODO: we might want to use an official logo here
-      enrollmentPermission: Permission.ENROLLYUBICO,
-      enrollmentActionLabel: $localize`Register`,
-      authenticationPrompt: $localize`Authenticate using your Yubikey token (YubiCloud)`,
-    },
-    {
-      type: TokenType.ASSIGN,
-      name: $localize`Assign token`,
-      description: $localize`Claim an existing token and link it to your user account`,
-      icon: 'link',
-      enrollmentPermission: Permission.ASSIGN,
-      enrollmentActionLabel: $localize`Assign`,
-    }];
-  }
-
-  get unknownTokenType(): TokenTypeDetails {
-    return {
-      type: TokenType.UNKNOWN,
-      name: $localize`Unknown Token`,
-      description: $localize`Unsupported token type`,
-      icon: 'apps',
-    };
-  }
-
   getTypeDetails(type: TokenType): TokenTypeDetails {
-    return this.tokenTypeDetails.find(td => td.type === type.toLowerCase()) || this.unknownTokenType;
+    return tokenTypeDetails.find(td => td.type === type.toLowerCase()) || unknownTokenTypeDetail;
   }
 
   getEnrollableTypes(): TokenTypeDetails[] {
-    return this.tokenTypeDetails.filter(t => t.enrollmentPermission);
+    return tokenTypeDetails.filter(t => t.enrollmentPermission);
   }
 
   handleError<T>(operation = 'operation', result?: T) {
--- src/app/api/token.ts
+++ src/app/api/token.ts
@@ -61,4 +61,105 @@ export interface EnrollToken {
   'yubico.tokenid'?: string;
 }
 
+export const tokenTypeDetails: TokenTypeDetails[] = [
+  {
+    type: TokenType.PASSWORD,
+    name: $localize`password token`,
+    description: $localize`Personal text-based secret`,
+    icon: 'keyboard',
+    enrollmentPermission: Permission.ENROLLPASSWORD,
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Enter the token password`,
+  },
+  {
+    type: TokenType.HOTP,
+    name: $localize`soft token (event)`,
+    description: $localize`Event-based soft token (HOTP)`,
+    icon: 'cached',
+    enrollmentPermission: Permission.ENROLLHOTP,
+    enrollmentType: 'googleauthenticator',
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Enter OTP from event-based soft token`,
+  },
+  {
+    type: TokenType.TOTP,
+    name: $localize`soft token (time)`,
+    description: $localize`Time-based soft token (TOTP)`,
+    icon: 'timelapse',
+    enrollmentPermission: Permission.ENROLLTOTP,
+    enrollmentType: 'googleauthenticator_time',
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Enter OTP from time-based soft token`,
+  },
+  {
+    type: TokenType.PUSH,
+    name: $localize`Push-Token`,
+    description: $localize`Confirm authentication requests on your smartphone with the Authenticator app`,
+    icon: 'screen_lock_portrait',
+    enrollmentPermission: Permission.ENROLLPUSH,
+    activationPermission: Permission.ACTIVATEPUSH,
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Confirm the authentication using your smartphone`,
+  },
+  {
+    type: TokenType.QR,
+    name: $localize`QR-Token`,
+    description: $localize`Use the Authenticator app to scan QR code authentication requests`,
+    icon: 'qr_code',
+    enrollmentPermission: Permission.ENROLLQR,
+    activationPermission: Permission.ACTIVATEQR,
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Confirm the authentication by scanning a QR code`,
+  },
+  {
+    type: TokenType.MOTP,
+    name: $localize`mOTP token`,
+    description: $localize`Generate OTPs from your mobile device given a secret password and a custom pin`,
+    icon: 'stay_current_portrait',
+    enrollmentPermission: Permission.ENROLLMOTP,
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Enter OTP from mOTP token`,
+  },
+  {
+    type: TokenType.SMS,
+    name: $localize`SMS token`,
+    description: $localize`Receive an OTP via SMS`,
+    icon: 'textsms',
+    enrollmentPermission: Permission.ENROLLSMS,
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Enter OTP delivered via SMS`,
+  },
+  {
+    type: TokenType.EMAIL,
+    name: $localize`email token`,
+    description: $localize`Receive an OTP via email`,
+    icon: 'email',
+    enrollmentPermission: Permission.ENROLLEMAIL,
+    enrollmentActionLabel: $localize`Create`,
+    authenticationPrompt: $localize`Enter OTP delivered via email`,
+  },
+  {
+    type: TokenType.YUBICO,
+    name: $localize`YubiCloud token`,
+    description: $localize`Register your Yubikey to authenticate against the YubiCloud.`,
+    icon: 'vpn_key', // TODO: we might want to use an official logo here
+    enrollmentPermission: Permission.ENROLLYUBICO,
+    enrollmentActionLabel: $localize`Register`,
+    authenticationPrompt: $localize`Authenticate using your Yubikey token (YubiCloud)`,
+  },
+  {
+    type: TokenType.ASSIGN,
+    name: $localize`Assign token`,
+    description: $localize`Claim an existing token and link it to your user account`,
+    icon: 'link',
+    enrollmentPermission: Permission.ASSIGN,
+    enrollmentActionLabel: $localize`Assign`,
+  }
+];
 
+export const unknownTokenTypeDetail: TokenTypeDetails = {
+  type: TokenType.UNKNOWN,
+  name: $localize`Unknown Token`,
+  description: $localize`Unsupported token type`,
+  icon: 'apps',
+};
