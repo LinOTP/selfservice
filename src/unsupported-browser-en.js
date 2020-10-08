be11b9c2a81962ae4f15e2a97cad67880400a5e8
Lukas Engelter <lukas.engelter@keyidentity.com>
2020-10-08 16:49:03 +0200

    fix: use es5 style multi line text for unsupported browser message
    This allows to correctly display the message also in the development server for unsupported browsers

--- src/unsupported-browser-en.js
+++ src/unsupported-browser-en.js
@@ -1,27 +1,27 @@
 isLegacy = /msie\s|trident\/|edge\//i.test(window.navigator.userAgent);
 
 if (isLegacy) {
-  document.body.innerHTML = `
-    <style>
-        body{
-            background: #f2f2f2;
-        }
-        .unsupported {
-            background: white;
-            border: 1px solid #d6d6d6;
-            margin: 35px 15px;
-            padding: 20px; 30px;
-        }
-    </style>
-    <div class="unsupported">
-        <h1>Unsupported Browser</h1>
-        <p>Your browser version is unsupported and the application does not work correctly.</p>
-        <p>The latest versions of following browsers are supported:</p>
-        <ul>
-          <li>Microsoft Edge</li>
-          <li>Mozilla Firefox</li>
-          <li>Google Chrome</li>
-        </ul>
-    </div>
-  `;
+  document.body.innerHTML = ' \
+    <style>\
+        body{\
+            background: #f2f2f2;\
+        }\
+        .unsupported {\
+            background: white;\
+            border: 1px solid #d6d6d6;\
+            margin: 35px 15px;\
+            padding: 20px; 30px;\
+        }\
+    </style>\
+    <div class="unsupported">\
+        <h1>Unsupported Browser</h1>\
+        <p>Your browser version is unsupported and the application does not work correctly.</p>\
+        <p>The latest versions of following browsers are supported:</p>\
+        <ul>\
+          <li>Microsoft Edge</li>\
+          <li>Mozilla Firefox</li>\
+          <li>Google Chrome</li>\
+        </ul>\
+    </div>\
+  ';
 }
