784eecdb6e7ed669a1198ecffcb5ed3d79d52df4
Radoslaw Grabos <Radoslaw.Grabos@netgo.de>
2024-04-25 08:28:46 +0200

    refactor: use set-pin component in oath create token step

--- src/app/enroll/enroll-oath-dialog/oath-enrollment/oath-enrollment.module.ts
+++ src/app/enroll/enroll-oath-dialog/oath-enrollment/oath-enrollment.module.ts
@@ -3,6 +3,7 @@ import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ReactiveFormsModule } from "@angular/forms";
 import { NgSelfServiceCommonModule } from "@app/common/common.module";
+import { TokenPinFormLayoutComponent } from "@app/enroll/token-pin-form-layout/token-pin-form-layout.component";
 import { MaterialModule } from "@app/material.module";
 import { QRCodeModule } from "angularx-qrcode";
 import { NgxPermissionsModule } from "ngx-permissions";
@@ -15,7 +16,7 @@ import { VerifyTokenComponent } from "./verify-token/verify-token.component";
 
 @NgModule({
     declarations: [CreateTokenStepComponent, FocusOnStepperChangeDirective, ImportTokenStepComponent, VerifyTokenComponent, DoneStepComponent, TokenInfoComponent],
-    imports: [CommonModule, MaterialModule, ReactiveFormsModule, NgSelfServiceCommonModule, A11yModule, QRCodeModule, NgxPermissionsModule.forChild()],
+    imports: [CommonModule, MaterialModule, ReactiveFormsModule, NgSelfServiceCommonModule, A11yModule, QRCodeModule, NgxPermissionsModule.forChild(), TokenPinFormLayoutComponent],
     exports: [CreateTokenStepComponent, FocusOnStepperChangeDirective, ImportTokenStepComponent, VerifyTokenComponent, DoneStepComponent],
     providers: [],
 })
