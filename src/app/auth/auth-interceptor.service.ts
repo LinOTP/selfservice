5eac7ca26eb2ff06e997a873750d46a3d3969786
Felix Neumann <felix.neumann@netgo.de>
2024-08-13 20:08:54 +0700

    feature: handle LinOTP "status": "false"

--- src/app/auth/auth-interceptor.service.ts
+++ src/app/auth/auth-interceptor.service.ts
@@ -1,24 +1,36 @@
-import { HttpErrorResponse, HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { HttpErrorResponse, HttpEvent, HttpHandler, HttpResponse , HttpInterceptor, HttpRequest } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 import { EMPTY, Observable, throwError } from 'rxjs';
-import { catchError } from 'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 
 import { LoginService } from '@app/login/login.service';
 import { Duration, NotificationService } from '@common/notification.service';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
-
-
   constructor(
     private loginService: LoginService,
-    private notificationService: NotificationService,
+    private notificationService: NotificationService
   ) { }
 
-  intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-
+  intercept(
+    req: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
     return next.handle(req).pipe(
+      tap((event: HttpEvent<any>) => {
+        if (
+          event instanceof HttpResponse &&
+          event.body?.result?.status === false
+        ) {
+          throw new HttpErrorResponse({
+            status: 500,
+            statusText: event.body.result.error.message,
+            url: req.urlWithParams,
+          });
+        }
+      }),
       catchError((error: HttpErrorResponse, caught) => {
         if (error.status === 401) {
           this.loginService.handleLogout(true);
@@ -29,14 +41,14 @@ export class AuthInterceptor implements HttpInterceptor {
             );
           }
           return EMPTY;
-        } else if (String(error.status).startsWith('5')) {
+        } else if (String(error.status).startsWith("5")) {
           this.notificationService.errorMessage(
             $localize`Server error encountered, request failed.`,
             Duration.LONG
           );
         }
         return throwError(() => error);
-      }));
+      })
+    );
   }
-
 }
