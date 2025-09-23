746f9de40b68101b94b1e71d63ced7111f64c6da
hamza.hodzic <hamza.hodzic@netgo.de>
2025-09-23 09:57:03 +0200

    refactor: done-step component

--- src/app/enroll/done-step/done-step.component.ts
+++ src/app/enroll/done-step/done-step.component.ts
@@ -1,28 +1,63 @@
-import { Component, Input } from "@angular/core";
+import { Component, Input, OnInit } from "@angular/core";
 import { TokenType } from "@api/token";
 import { EnrolledToken } from "@app/enroll/enroll-dialog-base.directive";
 import { Permission } from "@common/permissions";
 import { NgxPermissionsService } from "ngx-permissions";
+
+// Sadly we can not use a generic done step for all kinds of processes
+enum CompletionState {
+  ENROLLMENT_SUCCESS = 'ENROLLMENT_SUCCESS',
+  ACTIVATION_SUCCESS = 'ACTIVATION_SUCCESS',
+  ASSIGN_SUCCESS = 'ASSIGN_SUCCESS',
+  PUSH_QR_NEEDS_ACTIVATION = 'PUSH_QR_NEEDS_ACTIVATION',
+  PUSH_QR_NO_ACTIVATE_PERMISSION = 'PUSH_QR_NO_ACTIVATE_PERMISSION'
+}
 
 @Component({
 	selector: 'app-done-step',
   templateUrl: './done-step.component.html',
   styleUrls: ['done-step.component.scss']
 })
-export class DoneStepComponent {
-	@Input() token: EnrolledToken
+export class DoneStepComponent implements OnInit {
+	@Input({required: true}) token: EnrolledToken
   @Input() isAssignProcess: boolean = false;
+  @Input() isQRPushActivationProcess: boolean = false;
 
-  constructor(private permissionService: NgxPermissionsService) {
+  completionState: CompletionState
+
+  constructor(private permissionService: NgxPermissionsService) {}
+
+  ngOnInit(): void {
+    // Though the input being required it doesn't fully prevent the token being undefined
+    if(!this.token) return;
+    this.completionState = this.setCompletionState();
   }
 
-  isQRAndNoActivatePerm() {
-    return this.token?.type === TokenType.QR && !this.permissionService.getPermission(Permission.ACTIVATEQR)
-  }
+  private setCompletionState(): CompletionState {
+    const isQR = this.token.type === TokenType.QR;
+    const isPush = this.token.type === TokenType.PUSH;
+    const hasActivateQRPerm: string | undefined = this.permissionService.getPermission(Permission.ACTIVATEQR)?.name
+    const hasActivatePushPerm: string | undefined = this.permissionService.getPermission(Permission.ACTIVATEPUSH)?.name
 
-  isPushAndNoActivatePerm() {
-    return this.token?.type === TokenType.PUSH && !this.permissionService.getPermission(Permission.ACTIVATEPUSH)
+    switch (true) {
+      case this.isAssignProcess:
+        return CompletionState.ASSIGN_SUCCESS;
+
+      case this.isQRPushActivationProcess:
+        return CompletionState.ACTIVATION_SUCCESS;
+
+      case (isQR && !hasActivateQRPerm) || (isPush && !hasActivatePushPerm):
+        return CompletionState.PUSH_QR_NO_ACTIVATE_PERMISSION;
+
+      // TODO LINSELF-220 - delete this when activation is integrated in enroll
+      case (isQR && hasActivateQRPerm !== undefined) || (isPush && hasActivatePushPerm !== undefined):
+        return CompletionState.PUSH_QR_NEEDS_ACTIVATION;
+
+      default:
+        return CompletionState.ENROLLMENT_SUCCESS;
+    }
   }
 
   protected readonly TokenType = TokenType;
+  protected readonly CompletionState = CompletionState;
 }
